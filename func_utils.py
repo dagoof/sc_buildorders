52b7ca2d239aeb7d178c52237a78ad266dd80225
Peter DeGroff <dagoof@gmail.com>
2012-01-07 13:59:35 +1100

    biuld orders in thing completed

--- func_utils.py
+++ func_utils.py
@@ -10,3 +10,13 @@ def dict_create_path(path, into):
             this_section[index] = {}
         this_section = this_section[index]
     return this_section
+
+def map_sub(f, _iter):
+    _part = functools.partial(map_sub, f)
+    if isinstance(_iter, collections.Mapping):
+        k, v = zip(*_iter.items())
+        return dict(zip(k, _part(v)))
+    elif is_iterable(_iter):
+        return map(_part, _iter)
+    else:
+        return f(_iter)
